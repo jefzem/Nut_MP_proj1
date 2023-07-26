48c6a66130b9494d43956c60ac762ad2d6a6dbe4
Jean-francois Gomez <jeanfrancois.gomez@live.fr>
2023-07-26 13:46:32 +0200

    26071346

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-#hello1
+
 
 import os
 from flask import Flask, redirect, render_template, request, send_from_directory, url_for, jsonify
@@ -46,7 +46,47 @@ def api_entreprises():
 def simulations():
     print('simulations')
     return render_template('simulations.html')
+##### API Marketplace ##############
+@app.route('/info_contenants_V1')
+def info_contenants_V1():
+    print('info contenants V1')
+    return render_template('API/List_Contenants_References_V01.html')
 
+@app.route('/Contenants_enregistres_V1',methods=['GET'])
+def Contenants_enregistres_V1():
+    print('info contenants enregistresV1')
+    conn = sqlite3.connect('PILCv0.5.db')
+    ##conn.row_factory = dict_factory  <= rajoute le nom des colonnes
+    cur = conn.cursor()
+    all_Contenants = cur.execute('SELECT * FROM Contenants').fetchall()
+    return jsonify(all_Contenants)
+    #return render_template('API/Contenants_enregistrés_V1.html')
+
+@app.route('/Contenants_enregistres_parfab_V1',methods=['POST'])
+def Contenants_enregistres_parfab_V1():
+    print('info contenants enregistresV1 parfab_')
+    donnees = request.form
+    print (donnees)
+    print ("toto")
+    nom=donnees.get('nom')
+    print (nom)
+    conn = sqlite3.connect('PILCv0.5.db')
+    ##conn.row_factory = dict_factory  <= rajoute le nom des colonnes
+    cur = conn.cursor()
+    all_Contenants = cur.execute("SELECT * FROM Contenants WHERE fabricant = ?", (nom,)).fetchall()
+    #return "Traitement des données"
+    return jsonify(all_Contenants)
+    #return render_template('API/Contenants_enregistrés_V1.html')
+
+
+Contenants_enregistres_parfab_V1
+##### API Usagers ###########
+
+#### API  Entreprises ########
+
+##### API Simulations  ##########
+
+##### ANNEXES ########
 @app.route('/hello', methods=['POST'])
 def hello():
    name = request.form.get('name')
@@ -58,6 +98,10 @@ def hello():
        print('Request for hello page received with no name or blank name -- redirecting')
        return redirect(url_for('index'))
 
+@app.route('/login')
+def login():
+       return render_template('login.html')
 
+###### RUN ##############
 if __name__ == '__main__':
    app.run()
